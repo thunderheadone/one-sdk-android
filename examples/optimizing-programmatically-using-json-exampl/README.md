9f398c029c406305971f9c38a0aae2533a66c098
Matthew Frederick <mfrederick@thunderhead.com>
2019-06-08 22:22:26 -0400

    THX-39233: First round of PR feedback

--- examples/optimizing-programmatically-using-json-exampl/README.md
+++ examples/optimizing-programmatically-using-json-exampl/README.md
@@ -18,3 +18,17 @@
 The information entered in the `gradle.properties` file is used as as the initialization parameters for the Thunderhead SDK.
 The ThunderheadSDK is initialized in the `ExampleApplication.kt` class using the information you entered in the `gradle.properties` file.
 If your information is correct and there are no network errors the `MainActivity.kt` will be started when the App is opened.
+
+This example application is meant to be used while working through the optimization information found on the ONE Help site found at 
+https://eu2.thunderhead.com/one/help/conversations/how-do-i/mobile/one_integrate_mobile_exercise/. 
+
+The app is a simple TabView based app with 2 fragment screens:
+ * First Tab `FirstFragment`
+   * Contains a Recycler View which displays a list of product images
+   * 3 products are displayed in the list and are linked to images found under the app res folder 
+   * This will be the area which we will optimize based on JSON content received from ONE
+   * Tapping a recycler item cell in the second tab will send a response code to ONE to record the user sentiment for 
+     the optimization asset shown
+ * Second Tab `SecondFragment`
+   * Contains a simple set of buttons which will help us record the app user interest similar to how this could be 
+     achieved in your app
--- examples/optimizing-programmatically-using-json-exampl/app/src/main/java/com/example/optimizationapp/MainActivity.kt
+++ examples/optimizing-programmatically-using-json-exampl/app/src/main/java/com/example/optimizationapp/MainActivity.kt
@@ -29,37 +29,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fragmentAdapter = MyPagerAdapter(supportFragmentManager)
+        // create the tabs pager adapter
+        val fragmentAdapter = ActivityTabsPager(supportFragmentManager)
+
+        // get the view pager view from the layout and set the pager adapter
         viewPager.adapter = fragmentAdapter
 
+        // get the tab layout view from the layout and set the view pager view
         tabLayout.setupWithViewPager(viewPager)
     }
 
-    class MyPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    class ActivityTabsPager(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
-        override fun getItem(position: Int): Fragment {
-            return when (position) {
-                0 -> {
-                    FirstFragment()
-                }
-                else -> {
-                    return SecondFragment()
-                }
-            }
-        }
+        override fun getItem(position: Int): Fragment =
+            if (position == 0) FirstFragment() else SecondFragment()
 
-        override fun getCount(): Int {
-            return 2
-        }
+        override fun getCount(): Int = 2
 
-        override fun getPageTitle(position: Int): CharSequence {
-            return when (position) {
-                0 -> "First Tab"
-                else -> {
-                    return "Second Tab"
-                }
-            }
-        }
+        override fun getPageTitle(position: Int): CharSequence =
+            if (position == 0) "First Tab" else "Second Tab"
     }
 }
 
@@ -68,61 +56,59 @@ class MainActivity : AppCompatActivity() {
  */
 class FirstFragment : Fragment() {
 
-    private lateinit var viewAdapter: MyAdapter
-    private lateinit var viewManager: RecyclerView.LayoutManager
+    private val viewAdapter by lazy { FirstFragmentRecyclerViewAdapter() }
+    private val viewManager by lazy { LinearLayoutManager(context) }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val parent = inflater.inflate(R.layout.fragment_first, container, false)
 
-        viewManager = LinearLayoutManager(context)
-        viewAdapter = MyAdapter()
-
         // get optimization response from SDK
-        val one = One.getInstance(context?.applicationContext)
-        one.registerInteractionCallback(
-            "/FirstFragment"
-        ) { _, response ->
-            run {
-                one.processResponse(response)
-                parseData(response)
+        One.getInstance(context?.applicationContext)?.let { thunderhead ->
+            thunderhead.registerInteractionCallback(
+                "/FirstFragment-recycler_view_one"
+            ) { _, response ->
+                run {
+                    thunderhead.processResponse(response)
+                    parseData(response)
+                }
             }
         }
 
-        parent.findViewById<RecyclerView>(R.id.recycler_view_one).apply {
+        return parent.findViewById<RecyclerView>(R.id.recycler_view_one).apply {
             setHasFixedSize(true)
             layoutManager = viewManager
             adapter = viewAdapter
         }
-
-        return parent
     }
 
     // parse out optimization url
     private fun parseData(response: BaseResponse) {
-        if (response.optimizations.isNotEmpty()) {
-            for (optimization in response.optimizations) {
-                val data = optimization.data.toByteArray(Charsets.UTF_8)
-                val bytes = Base64.decode(data, Base64.DEFAULT)
-                val jsonObject = JSONObject(String(bytes, Charsets.UTF_8))
-                if (jsonObject.has("actions")) {
-                    val action = jsonObject.getJSONArray("actions").get(0) as JSONObject
-                    if (action.getString("name").contains("banner")) {
-                        val content = action.getJSONObject("asset").getString("content")
-                        val contentJson = JSONObject(Html.fromHtml(content).toString())
-                        viewAdapter.bannerUrl = contentJson.getString("image")
-                        viewAdapter.notifyDataSetChanged()
-                    } else if (action.getString("name").contains("card")) {
-                        val content = action.getJSONObject("asset").getString("content")
-                        val contentJson = JSONObject(Html.fromHtml(content).toString())
-                        viewAdapter.cardUrl = contentJson.getString("image")
-                        viewAdapter.notifyDataSetChanged()
-                    }
-                }
-            }
-        }
+        val actions = response.optimizations
+            .map { op -> op.data.toByteArray(Charsets.UTF_8) }
+            .map { data -> Base64.decode(data, Base64.DEFAULT) }
+            .map { bytes -> JSONObject(String(bytes, Charsets.UTF_8)) }
+            .filter { json -> json.has("actions") }
+            .map { json -> json.getJSONArray("actions").get(0) as JSONObject }
+
+        actions.filter { it.getString("name").contains("banner") }
+            .map { it.getJSONObject("asset").getString("content") }
+            .map { content -> JSONObject(Html.fromHtml(content).toString()) }
+            .forEach { contentJson -> performUpdate(contentJson, true) }
+
+        actions.filter { it.getString("name").contains("card") }
+            .map { it.getJSONObject("asset").getString("content") }
+            .map { content -> JSONObject(Html.fromHtml(content).toString()) }
+            .forEach { contentJson -> performUpdate(contentJson, false) }
     }
 
-    class MyAdapter : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+    private fun performUpdate(json: JSONObject, isBanner: Boolean) {
+        val url = json.getString("image")
+        if (isBanner) viewAdapter.bannerUrl = url else viewAdapter.cardUrl = url
+        viewAdapter.notifyDataSetChanged()
+    }
+
+    class FirstFragmentRecyclerViewAdapter :
+        RecyclerView.Adapter<FirstFragmentRecyclerViewAdapter.FirstFragmentViewHolder>() {
 
         private val imageList = arrayListOf(
             R.drawable.product_1a,
@@ -133,31 +119,29 @@ class FirstFragment : Fragment() {
         var bannerUrl = ""
         var cardUrl = ""
 
-        class MyViewHolder(val image: ImageView) : RecyclerView.ViewHolder(image)
+        class FirstFragmentViewHolder(val image: ImageView) : RecyclerView.ViewHolder(image)
 
         override fun onCreateViewHolder(
             parent: ViewGroup,
             viewType: Int
-        ): MyViewHolder {
+        ): FirstFragmentViewHolder {
 
             val image = LayoutInflater.from(parent.context)
                 .inflate(
                     R.layout.list_image_cell
                     , parent, false
                 ) as ImageView
-            return MyViewHolder(image)
+            return FirstFragmentViewHolder(image)
         }
 
         // if optimized image is available, show it. Otherwise, show stock image
-        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        override fun onBindViewHolder(holder: FirstFragmentViewHolder, position: Int) {
             if (position == 0 && bannerUrl.isNotEmpty()) {
                 Glide.with(holder.image.context).load(bannerUrl).into(holder.image)
             } else if (position == 1 && cardUrl.isNotEmpty()) {
                 Glide.with(holder.image.context).load(cardUrl).into(holder.image)
             } else {
                 holder.image.setImageDrawable(ContextCompat.getDrawable(holder.image.context, imageList[position]))
-            }
-            holder.image.setOnClickListener {
             }
         }
 
@@ -171,42 +155,39 @@ class FirstFragment : Fragment() {
 class SecondFragment : Fragment() {
 
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
-    private lateinit var viewManager: RecyclerView.LayoutManager
+    private val viewManager by lazy { LinearLayoutManager(context) }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val parent = inflater.inflate(R.layout.fragment_second, container, false)
 
-        viewManager = LinearLayoutManager(context)
-        viewAdapter = MyAdapter(resources.getStringArray(R.array.list_items))
+        viewAdapter = SecondFragmentRecyclerViewAdapter(resources.getStringArray(R.array.list_items))
 
-        parent.findViewById<RecyclerView>(R.id.recycler_view_two).apply {
+        return parent.findViewById<RecyclerView>(R.id.recycler_view_two).apply {
             setHasFixedSize(true)
             layoutManager = viewManager
             adapter = viewAdapter
         }
-
-        return parent
     }
 
-    class MyAdapter(private val dataset: Array<String>) :
-        RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+    class SecondFragmentRecyclerViewAdapter(private val dataset: Array<String>) :
+        RecyclerView.Adapter<SecondFragmentRecyclerViewAdapter.SecondFragmentViewHolder>() {
 
-        class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+        class SecondFragmentViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 
         override fun onCreateViewHolder(
             parent: ViewGroup,
             viewType: Int
-        ): MyViewHolder {
+        ): SecondFragmentViewHolder {
 
             val textView = LayoutInflater.from(parent.context)
                 .inflate(
                     android.R.layout.simple_list_item_1
                     , parent, false
                 ) as TextView
-            return MyViewHolder(textView)
+            return SecondFragmentViewHolder(textView)
         }
 
-        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        override fun onBindViewHolder(holder: SecondFragmentViewHolder, position: Int) {
             holder.textView.text = dataset[position]
             holder.textView.textSize = 25f
             holder.textView.setOnClickListener {
